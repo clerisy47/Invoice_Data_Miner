09dfa6d30e036e66c055ba6bf157fe43071a64ea
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2024-01-20 21:47:53 +0545

    update prompt

--- main.py
+++ main.py
@@ -6,8 +6,23 @@ import pytesseract
 import cv2
 
 DEFAULT_PROMPT = """
-
-
+Please retrieve title, invoive number, issue_date, total amount and table.   
+    Always return your response as a valid JSON string. The format of that string should be this, 
+    {
+        invoice_number: RSN/18-19/4126,
+        invoice_number_bbox: [608, 130, 720, 142],
+        invoice_date: 09/03/2019,
+        total: 76900,
+        table: S Description of Goods HSN/SAC GST Quantity Rate per Disc. % Amount
+                Rate
+                SAMSUNG S10PLUS 85171290 12 % 1 No. 68,660.71 No. 68,660.71
+                512GB G975 CERAMIC BLACK
+                Batch : 1 No.
+    }
+    If you can't find the information from this article 
+    then return "". Do not make things up. 
+    Here's the raw data from the invoice:
+    
 """
 MODEL = "gpt-3.5-turbo"
 
@@ -20,7 +35,7 @@ def image_to_text(image_path):
     text = pytesseract.image_to_string(image)
     return text
 
-def main(text):
+def text_to_df(text):
     final_prompt = DEFAULT_PROMPT + text
     response = openapi.ChatCompletion.create(
         model=MODEL,
@@ -29,5 +44,8 @@ def main(text):
     content = response.messages[0]["content"]
     try:
         data = json.loads(content)
+        data = {key: [value] for key, value in data.items()}
+        return pd.DataFrame(data)
+
     except (json.JSONDecodeError, IndexError):
         pass
